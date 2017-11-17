af36765c15e469c17cb23002ad1c8efd862cd168
Brad Larson <bhlarson@gmail.com>
2017-11-16 20:17:32 -0800

    Minimum project for debugging

--- server.js
+++ server.js
@@ -3,78 +3,88 @@ require('dotenv').config({ path: './config.env' });
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var ioSocket; 
-var ov1 = require('./OV1');
-var mysql = require('mysql');
-var log4js = require('log4js');
+
+SerialPort = require('serialport');
+portName = '/dev/ttyAMA0';
+settings = { baudrate: 115200, databits: 8, stopbits: 1, parity: 'none' };
+//var io = require('socket.io')(http);
+//var ioSocket; 
+//var ov1 = require('./OV1');
+//var mysql = require('mysql');
+//var log4js = require('log4js');
 console.log("Dependancies Found");
 
-log4js.configure({
-    appenders: { command: { type: 'file', filename: 'state.log' } },
-    categories: { default: { appenders: ['command'], level: 'ALL' } }
-});
-const cmdLog = log4js.getLogger('command');
+port = new SerialPort(portName, settings);
 
-var ov1Port = new ov1.OVPort();
-ov1Port.Output(function (data) {
-    console.log(data);
-    if (data.serialData && ioSocket) {
-        ioSocket.emit('data', data.serialData);
-    }
+port.on('data', function (data) {
+    console.log('Data:', data);
 });
-var config = { log: cmdLog, portName: process.env.serialport };
-ov1Port.Start(config, function (result) {
-    console.log(result);
-});
+
+//log4js.configure({
+//    appenders: { command: { type: 'file', filename: 'state.log' } },
+//    categories: { default: { appenders: ['command'], level: 'ALL' } }
+//});
+//const cmdLog = log4js.getLogger('command');
+
+//var ov1Port = new ov1.OVPort();
+//ov1Port.Output(function (data) {
+//    console.log(data);
+//    if (data.serialData && ioSocket) {
+//        ioSocket.emit('data', data.serialData);
+//    }
+//});
+//var config = { log: cmdLog, portName: process.env.serialport };
+//ov1Port.Start(config, function (result) {
+//    console.log(result);
+//});
  
-command = { name: 'VRSN' };
-ov1Port.Input(command);
+//command = { name: 'VRSN' };
+//ov1Port.Input(command);
 
-// Home database credentials
-var pool = mysql.createPool({
-    connectionLimit: 10,
-    host: process.env.dbhost,
-    user: process.env.dbuser,
-    //  password        : 'password',
-    database: process.env.dbname
-});
+//// Home database credentials
+//var pool = mysql.createPool({
+//    connectionLimit: 10,
+//    host: process.env.dbhost,
+//    user: process.env.dbuser,
+//    //  password        : 'password',
+//    database: process.env.dbname
+//});
 
-console.log("mysql.createPool exists=" + (typeof pool !== 'undefined'));
+//console.log("mysql.createPool exists=" + (typeof pool !== 'undefined'));
 
-var port = Number(process.env.nodeport) || 1337;
-app.use(express.static('public'));
+//var port = Number(process.env.nodeport) || 1337;
+//app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-    res.sendFile('index.html')
-});
+//app.get('/', function (req, res) {
+//    res.sendFile('index.html')
+//});
 
 http.listen(port, function () {
     console.log('Socket.IO listening on port ' + port);
 });
 
-io.on('connection', function (socket) {
-    socket.broadcast.emit('Server Connected');
-    ioSocket = socket;
-    socket.on('disconnect', function () {
-        console.log('Socket.IO  disconnected ' + socket.id);
-    });
-    socket.on('connect_failed', function () {
-        console.log('socket.io connect_failed');
-    })
-    socket.on('reconnect_failed', function () {
-        console.log('socket.io reconnect_failed');
-    })
-    socket.on('error', function (err) {
-        console.log('socket.io error:' + err);
-    })
-    socket.on('Command', function (data) {
-        command = { name: data.cmd, value: data.val };
-        ov1Port.Input(command);
+//io.on('connection', function (socket) {
+//    socket.broadcast.emit('Server Connected');
+//    ioSocket = socket;
+//    socket.on('disconnect', function () {
+//        console.log('Socket.IO  disconnected ' + socket.id);
+//    });
+//    socket.on('connect_failed', function () {
+//        console.log('socket.io connect_failed');
+//    })
+//    socket.on('reconnect_failed', function () {
+//        console.log('socket.io reconnect_failed');
+//    })
+//    socket.on('error', function (err) {
+//        console.log('socket.io error:' + err);
+//    })
+//    socket.on('Command', function (data) {
+//        command = { name: data.cmd, value: data.val };
+//        ov1Port.Input(command);
 
-        console.log('Command ' + JSON.stringify(data));
-    });
-});
+//        console.log('Command ' + JSON.stringify(data));
+//    });
+//});
 
 module.exports = app;
 console.log("GridInsightOIV-1 Started");
